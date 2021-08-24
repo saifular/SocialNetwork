0a91eee8dd03f8b0836f9df57e240b012a082793
Saiful Islam <byteroni@gmail.com>
2021-08-25 03:02:40 +0600

    User Registration Route (Express)

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const db=config.get('mongoURI');
 
 const connectDb= async()=>{
   try{
-      await mongoose.connect(db,{useNewUrlParser:true});
+      await mongoose.connect(db,{useNewUrlParser:true,useUnifiedTopology: true});
       console.log('Database Connect...');
 
   }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {check,validationResult}=require('express-validator/check');
-
+const {check,validationResult}=require('express-validator');
+const User =require('../../models/User');
+const gravatar=require('gravatar');
+const bcrypt=require('bcryptjs');
 router.post('/',[
     check('name','Name is required.')
     .not()
@@ -11,14 +13,46 @@ router.post('/',[
     check('password','Please enter a passsword with 6 nor more characters')
     .isLength({min: 6})
 
-], (req,res) => {
+], async(req,res) => {
     const errors = validationResult(req);
     console.log(req.body);
     if(!errors.isEmpty()){
         return res.status(400).json({errors:errors.array()});
        
     }
-    res.send('User Route')
+    const {name,email,password}=req.body;
+    try{
+        let user=await User.findOne({email});
+        if(user){
+            res.status(400).json({errors:[{meg:'User Already Exists'}]});
+        }
+        const avatar =gravatar.url(email,{
+            s:'200',
+            r:'pg',
+            d:'mm'
+
+        })
+        user=new User({
+            name,
+            email,
+            avatar,
+            password
+
+        });
+        const salt=await bcrypt.genSalt(10);
+        user.password=await bcrypt.hash(password,salt);
+        await user.save();
+
+
+    res.send('User Register')
+    }
+    catch(err){
+        console.error(err.message);
+        res.status(500).send('Server error');
+
+    }
+
+
 
 });
 
