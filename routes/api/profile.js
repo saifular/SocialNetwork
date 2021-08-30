5056707809683c38e2ca4dbbe49538ae462645db
Saiful Islam <byteroni@gmail.com>
2021-08-31 02:12:13 +0600

    put experience on profile api with express

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -137,4 +137,61 @@ router.get('/me', auth, async (req, res) => {
       }
     });
 
+    router.put(
+      '/experience',
+      [ auth,
+        [
+      check('title', 'Title is required')
+      .not()
+      .isEmpty(),
+      check('company', 'Company is required')
+      .not()
+      .isEmpty(),
+      check('from', 'From date is required and needs to be from the past')
+      .not()
+      .isEmail()
+          ]
+  ],
+      
+      async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+        }
+        const{
+          title,
+          company,
+          location,
+          from,
+          to,
+          current,
+          description
+
+        }=req.body;
+        const newExp={
+          title,
+          company,
+          location,
+          from,
+          to,
+          current,
+          description
+
+        }
+    
+        try {
+          const profile = await Profile.findOne({ user: req.user.id });
+    
+          profile.experience.unshift(newExp);
+    
+          await profile.save();
+    
+          res.json(profile);
+        } catch (err) {
+          console.error(err.message);
+          res.status(500).send('Server Error');
+        }
+      }
+    );
+
 module.exports=router;
