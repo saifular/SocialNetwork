6e07a463d376e9fd91f95249e4b67fa04feb03c0
Saiful Islam <byteroni@gmail.com>
2021-09-01 01:33:25 +0600

    add education on profile with express

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -149,7 +149,7 @@ router.get('/me', auth, async (req, res) => {
       .isEmpty(),
       check('from', 'From date is required and needs to be from the past')
       .not()
-      .isEmail()
+      .isEmpty()
           ]
   ],
       
@@ -206,5 +206,67 @@ router.get('/me', auth, async (req, res) => {
         return res.status(500).json({ msg: 'Server error' });
       }
     });
+
+    router.put(
+      '/education',
+      [ auth,
+        [
+      check('school', 'School is required')
+      .not()
+      .isEmpty(),
+      check('degree', 'Degree is required')
+      .not()
+      .isEmpty(),
+      check('fieldofstudy', 'Field of study is required')
+      .not()
+      .isEmpty(),
+      check('from', 'From date is required and needs to be from the past')
+      .not()
+      .isEmpty()
+          ]
+  ],
+      
+      async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+        }
+        const{
+          school,
+          degree,
+          fieldofstudy,
+          from,
+          to,
+          current,
+          description
+
+        }=req.body;
+        const newEdu={
+          school,
+          degree,
+          fieldofstudy,
+          from,
+          to,
+          current,
+          description
+
+        }
+    
+        try {
+          const profile = await Profile.findOne({ user: req.user.id });
+    
+          profile.education.unshift(newEdu);
+    
+          await profile.save();
+    
+          res.json(profile);
+        } catch (err) {
+          console.error(err.message);
+          res.status(500).send('Server Error');
+        }
+      }
+    );
+
+   
 
 module.exports=router;
