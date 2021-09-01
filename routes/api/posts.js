ccc3b13a1588eaf4264ea1fb23b4ad1c37abcaa6
Saiful Islam <byteroni@gmail.com>
2021-09-02 00:57:24 +0600

    Get All Post with Express

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -35,6 +35,16 @@ router.post(
       }
     }
   );
+  router.get('/', auth, async (req, res) => {
+    try {
+      const posts = await Post.find().sort({ date: -1 });
+      res.json(posts);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  });
+  
 
 
 module.exports=router;
