324db7ef1cc70b944491c6d0d3be57ba3dbeab64
Saiful Islam <byteroni@gmail.com>
2021-09-11 22:02:02 +0600

    User Register with Redux

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,9 +2,10 @@ import React, { Fragment,useState } from 'react';
 import {Link} from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
  
-const Register = ({setAlert})=> {
+const Register = ({setAlert,register})=> {
   const [fromData, setFromData] = useState({
       name:'',
       email:'',
@@ -19,7 +20,7 @@ const Register = ({setAlert})=> {
         setAlert('Password do not match', 'danger');
       }
       else{
-          console.log(fromData);
+         register({name,email,password});
       }
   }
   
@@ -31,7 +32,7 @@ const Register = ({setAlert})=> {
       <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
       <form onSubmit={e=>onSubmit(e)} className="form">
         <div className="form-group">
-          <input type="text" placeholder="Name" name='name' value={name} onChange={e=>onChange(e)} required />
+          <input type="text" placeholder="Name" name='name' value={name} onChange={e=>onChange(e)}  />
         </div>
         <div className="form-group">
           <input type="email" placeholder="Email Address" value={email} onChange={e=>onChange(e)} name='email' />
@@ -41,10 +42,10 @@ const Register = ({setAlert})=> {
           </small>
         </div>
         <div className="form-group">
-          <input type="password" placeholder="Password" value={password} onChange={e=>onChange(e)} name='password' minlength="6" />
+          <input type="password" placeholder="Password" value={password} onChange={e=>onChange(e)} name='password' />
         </div>
         <div className="form-group">
-          <input type="password" name='password2' value={password2} onChange={e=>onChange(e)} placeholder="Confirm Password" minlength="6" />
+          <input type="password" name='password2' value={password2} onChange={e=>onChange(e)} placeholder="Confirm Password"  />
         </div>
         <input type="submit" value="Register"  className="btn btn-primary" />
       </form>
@@ -56,6 +57,7 @@ const Register = ({setAlert})=> {
 };
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired
 
 };
 
@@ -64,4 +66,4 @@ const mapStateToProps = (state) => ({
 
 
 
-export default connect(mapStateToProps, {  setAlert })(Register);
+export default connect(mapStateToProps, {  setAlert ,register})(Register);
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 import alert from './alert';
-
+import register from './auth';
 export default combineReducers({
-    alert
+    alert,
+    register
  
 });
